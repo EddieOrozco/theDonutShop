c845142e5d63c8ff5e38e6d28940027059107d90
EddieOrozco <orozcoe230@gmail.com>
2021-03-31 19:51:08 -0500

    Removing and modifying pages / added donut images

---
+++ js/app.js
@@ -0,0 +1,59 @@
+// let data = {
+//   donuts: [
+//     {
+//       id: 1,
+//       name: 'Iced Ring',
+//       description: 'Donut hole topped with chocolate frosting. ',
+//       image: '',
+//     },
+//     {
+//       id: 2,
+//       name: 'Blueberry',
+//       description: 'Blueberry cake infused donut.',
+//       image: '',
+//     },
+//     {
+//       id: 3,
+//       name: 'Toasted Coconut',
+//       description: 'Soft glazed topped toasted coconut donut.',
+//       image: '',
+//     },
+//     {
+//       id: 4,
+//       name: 'Sour Cream',
+//       description: 'Old school style donut soft to the touch.',
+//       image: '',
+//     },
+//     {
+//       id: 5,
+//       name: 'Frosted Red Velvet',
+//       description: 'Red velvet toped with vanilla frosting.',
+//       image: '',
+//     },
+//     {
+//       id: 6,
+//       name: 'Powder Cinnamon',
+//       description: 'Jelly filled donut with powder cinnamon.',
+//       image: '',
+//     },
+//     {
+//       id: 7,
+//       name: 'Maple',
+//       description:
+//         'Great with a bacon filled breakfast the famous Maple donut.',
+//       image: '',
+//     },
+//   ],
+// };
+
+// function myFunction() {
+//   let button = document.getElementsByClassName('navbar-toggler')[0];
+//   if (button.style.display === 'none') {
+//     button.style.display = 'block';
+//   } else {
+//     button.style.display = 'none';
+//   }
+// }
+// myFunction();
+
+console.log(button);
